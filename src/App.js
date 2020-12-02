d38030211ca2bb65e555392f2ece06dffb4a5295
front-kyj <youngjun.kim@genesislab.ai>
2020-12-02 14:52:30 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const TestFunc = () => {
+    console.log('????????TEST');
+    console.log('????????TEST');
+    return '수정바랍니다.';
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
