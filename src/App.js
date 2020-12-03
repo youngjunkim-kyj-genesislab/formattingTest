14f73ea56abe5e4d03d3611bc6266ad3c5760e5d
front-kyj <youngjun.kim@genesislab.ai>
2020-12-03 13:39:46 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './pages/Todo.js';
 
 function App() {
-  const TestFunc = () => {
-    console.log('????????TEST');
-    console.log('????????TEST');
-    return '수정바랍니다.';
-  };
-
-  const list = [1, 2, 3, 4, 5];
-  const foo = () => {
-    for (let i = 0; i < 10; i++) {
-      console.log(i);
-    }
-    return list.map((item, idx) => <div>{item}</div>);
-  };
-
-  const test = 'test';
-
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
     </div>
   );
 }
